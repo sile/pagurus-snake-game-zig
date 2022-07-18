eac2b4b2523dbcac13257c6cb2953f8147df3ec2
Takeru Ohta <phjgt308@gmail.com>
2022-07-19 00:19:28 +0900

    [WIP] Add event parsing

--- examples/snake/src/main.zig
+++ examples/snake/src/main.zig
@@ -3,9 +3,11 @@ const assets = @import("assets.zig");
 const SnakeGame = @import("game.zig").SnakeGame;
 const System = @import("system.zig").System;
 
+var GBA = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = GBA.allocator();
+
 export fn gameNew() *anyopaque {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+    //const allocator = GBA.allocator();
     const game = allocator.create(SnakeGame) catch @panic("failed to allocate SnakeGame");
     return @ptrCast(*anyopaque, game);
 }
@@ -20,27 +22,49 @@ export fn gameInitialize(game_ptr: *anyopaque) ?*anyopaque {
     return null; // no error
 }
 
-export fn gameHandleEvent(game: *anyopaque, event_bytes_ptr: *anyopaque, data_ptr: *anyopaque) ?*anyopaque {
+const WindowEvent = struct { redrawNeeded: struct { size: struct { width: u32, height: u32 } } };
+
+const Event = struct { window: WindowEvent };
+
+export fn gameHandleEvent(game: *anyopaque, event_bytes_ptr: *anyopaque, data_ptr: ?*anyopaque) ?*anyopaque {
     var system = System{};
     system.consoleLog("gameHandleEvent: 0");
+
+    const event_bytes = @ptrCast(*Bytes, @alignCast(@typeInfo(*Bytes).Pointer.alignment, event_bytes_ptr));
+    system.consoleLog(event_bytes.inner);
+
+    var stream = std.json.TokenStream.init(event_bytes.inner);
+    _ = std.json.parse(Event, &stream, .{}) catch {
+        system.consoleLog("failed to parse JSON");
+        @panic("failed to parse Event JSON (TODO");
+    };
+    system.consoleLog("gameHandleEvent: 1");
+
     _ = game;
-    _ = event_bytes_ptr;
     _ = data_ptr;
     return null;
     //unreachable;
 }
 
-export fn memoryAllocateBytes(size: usize) ?*anyopaque {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+// TODO: move
+const Bytes = struct { inner: []u8 };
 
-    var bytes = allocator.alloc(u8, size) catch return null;
-    return &bytes;
+export fn memoryAllocateBytes(size: usize) ?*anyopaque {
+    var system = System{};
+    system.consoleLog("memoryAllocateBytes");
+
+    var inner = allocator.alloc(u8, size) catch return null;
+    var bytes = allocator.create(Bytes) catch return null;
+    bytes.inner = inner;
+
+    return bytes;
 }
 
 export fn memoryBytesOffset(bytes_ptr: *anyopaque) *u8 {
-    const bytes = @ptrCast(*[]u8, @alignCast(@typeInfo(*[]u8).Pointer.alignment, bytes_ptr));
-    return @ptrCast(*u8, bytes.ptr);
+    // const bytes = @ptrCast(*[]u8, @alignCast(@typeInfo(*[]u8).Pointer.alignment, bytes_ptr));
+    // return @ptrCast(*u8, bytes.ptr);
+    const bytes = @ptrCast(*Bytes, @alignCast(@typeInfo(*Bytes).Pointer.alignment, bytes_ptr));
+    return @ptrCast(*u8, bytes.inner.ptr);
 }
 
 export fn memoryBytesLen(bytes_ptr: *anyopaque) usize {
@@ -49,9 +73,10 @@ export fn memoryBytesLen(bytes_ptr: *anyopaque) usize {
 }
 
 export fn memoryFreeBytes(bytes_ptr: *anyopaque) void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+    var system = System{};
+    system.consoleLog("memoryFreeBytes");
 
+    //    const allocator = GBA.allocator();
     const bytes = @ptrCast(*[]u8, @alignCast(@typeInfo(*[]u8).Pointer.alignment, bytes_ptr));
     allocator.free(bytes.*);
 }
