c57cb5abba89274f27a8fb196b2a2618984a2c70
Takeru Ohta <phjgt308@gmail.com>
2022-07-19 13:43:43 +0900

    Add spatial types

--- examples/snake/src/assets.zig
+++ examples/snake/src/assets.zig
@@ -1,3 +1,27 @@
 pub const IMG_BACKGROUND = @embedFile("../assets/background.rawrgba");
 
-pub const Assets = struct {};
+// TODO: move
+pub const Size = struct { width: u32, hight: u32 };
+pub const Position = struct { x: i32, y: i32 };
+pub const Region = struct { position: Position, size: Size };
+
+// TODO: move
+pub const Sprite = struct { image_data: []u8, image_size: Size, sprite_region: Region };
+
+pub fn createSprite(image_data: []u8, image_size: Size, sprite_position: Position, sprite_size: Size) Sprite {
+    return .{ .image_data = image_data, .image_size = image_size, .sprite_region = .{ .position = sprite_position, .size = sprite_size } };
+}
+
+pub fn xy(x: i32, y: i32) Position {
+    return .{ .x = x, .y = y };
+}
+
+pub fn wh(width: u32, height: u32) Size {
+    return .{ .width = width, .height = height };
+}
+
+pub fn square(size: u32) Size {
+    return wh(size, size);
+}
+
+pub const BACKGROUND: Sprite = createSprite(IMG_BACKGROUND, square(384), xy(0, 0), square(384));
