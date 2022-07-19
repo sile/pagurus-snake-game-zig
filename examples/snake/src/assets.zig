0740e744b9478c3d25fdf19aab22923df9882ef9
Takeru Ohta <phjgt308@gmail.com>
2022-07-19 16:25:59 +0900

    [WIP] Add Canvas

--- examples/snake/src/assets.zig
+++ examples/snake/src/assets.zig
@@ -6,7 +6,12 @@ pub const Position = struct { x: i32, y: i32 };
 pub const Region = struct { position: Position, size: Size };
 
 // TODO: move
-pub const Sprite = struct { image_data: []const u8, image_size: Size, sprite_region: Region };
+pub const Sprite = struct {
+    // RGBA
+    image_data: []const u8,
+    image_size: Size,
+    sprite_region: Region,
+};
 
 pub fn createSprite(image_data: []const u8, image_size: Size, sprite_position: Position, sprite_size: Size) Sprite {
     return .{ .image_data = image_data, .image_size = image_size, .sprite_region = .{ .position = sprite_position, .size = sprite_size } };
@@ -23,5 +28,18 @@ pub fn wh(width: u32, height: u32) Size {
 pub fn square(size: u32) Size {
     return wh(size, size);
 }
+
+pub const Canvas = struct {
+    // RGB
+    image_data: []const u8,
+    image_size: Size,
+
+    pub fn view(self: Canvas, region: Region) !CanvasView {
+        // TODO: validate
+        return CanvasView{ .canvs = self, .region = region };
+    }
+};
+
+pub const CanvasView = struct { canvas: Canvas, region: Region };
 
 pub const BACKGROUND: Sprite = createSprite(IMG_BACKGROUND.*[0..], square(384), xy(0, 0), square(384));
